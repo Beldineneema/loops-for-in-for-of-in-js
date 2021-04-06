b9c6dc3b3b241dc3425f7101c1b0a7a447045492
Beldineneema <beldineneema@gmail.com>
2021-04-06 09:09:56 +0300

    loops.js

---
+++ loops.js
@@ -0,0 +1,17 @@
+const names = ["tables", "chair", "phone", "book"];
+
+//for (const n of names) {
+    //console.log(n);
+//}
+
+  const symbols = {
+    t: "tables",
+    c: "chair",
+    p: "phone",
+    b: "book",
+
+};
+
+for(const n in symbols) {
+console.log(n);
+}
